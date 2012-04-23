669805ce13513f920b2d86156cfb85ad49eab507
Evan Davey <evan.j.davey@gmail.com>
2012-04-23 14:44:47 +0100

    uses django-tables2 for better output

--- bankdownloads/urls.py
+++ bankdownloads/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import patterns, url, include
 from django.views.generic import ListView
 from bankdownloads.models import BankDownload
-from django.contrib.auth.decorators import login_required, permission_required
 
 urlpatterns = patterns('',
-    (r'^$', login_required(ListView.as_view(
-        model=BankDownload,
-    ))),
+    (r'^$', bankdownloads.views.index),
 )
