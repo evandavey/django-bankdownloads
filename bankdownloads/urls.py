dcdca0ded82969eb702c4d0b9333771f347d1063
Evan Davey <evan.j.davey@gmail.com>
2012-04-23 14:47:01 +0100

    uses django-tables2 for better output

--- bankdownloads/urls.py
+++ bankdownloads/urls.py
@@ -3,5 +3,5 @@ from django.views.generic import ListView
 from bankdownloads.models import BankDownload
 
 urlpatterns = patterns('',
-    (r'^$', bankdownloads.views.index),
+    (r'^$', 'bankdownloads.views.index'),
 )
