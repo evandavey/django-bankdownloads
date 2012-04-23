1dbf91d614beb090ee5fcbe09248402b38945c3b
Evan Davey <evan.j.davey@gmail.com>
2012-04-23 13:54:44 +0100

    requiring login

--- bankdownloads/urls.py
+++ bankdownloads/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url, include
 from django.views.generic import ListView
 from bankdownloads.models import BankDownload
+from django.contrib.auth.decorators import login_required, permission_required
 
 urlpatterns = patterns('',
-    (r'^$', ListView.as_view(
+    (r'^$', login_required(ListView.as_view(
         model=BankDownload,
-    )),
+    ))),
 )
