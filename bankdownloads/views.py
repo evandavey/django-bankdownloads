7a934e0c6a1aaf196329459a8f94c6d45c707d75
Evan Davey <evan.j.davey@gmail.com>
2012-04-23 14:46:12 +0100

    uses django-tables2 for better output

---
+++ bankdownloads/views.py
@@ -0,0 +1,31 @@
+import django_tables2 as tables
+from django_tables2.utils import A  # alias for Accessor
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response,redirect
+from django.template import RequestContext
+
+from bankdownloads.models import BankDownload
+
+class BankDownloadTable(tables.Table):
+    
+    
+    name = tables.LinkColumn('bankdownload_detail', args=[A('pk')])
+    
+    
+    class Meta:
+        model = Recipe
+        exclude = []
+        
+
+@login_required(login_url='/accounts/login')   
+def index(request):
+
+    table = BankDownloadTable(BankDownload.objects.all())
+    table.paginate(page=request.GET.get("page", 1))
+    table.order_by = request.GET.get("sort")
+
+    ct={'table':table,
+    }
+
+    return render_to_response('bankdownload_list.html',ct,context_instance=RequestContext(request))
