76cc73367029255154cb4bdef70c6e38f9343aec
Evan Davey <evan.j.davey@gmail.com>
2012-04-23 13:43:34 +0100

    adds simple frontend

---
+++ bankdownloads/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url, include
+from django.views.generic import ListView
+from bankdownloads.models import BankDownload
+
+urlpatterns = patterns('',
+    (r'^/$', ListView.as_view(
+        model=BankDownload,
+    )),
+)
